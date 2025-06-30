25b8b85d9032509404160fd3a3de91a84850ef3c
SanyaSajid <pc6054.sanya@gmail.com>
2025-06-30 23:53:15 +0500

    updated

--- app/studio/[[...tool]]/page.tsx
+++ app/studio/[[...tool]]/page.tsx
@@ -3,8 +3,6 @@
 import { Studio } from 'sanity'
 import config from '../../../sanity.config'
 
-export { metadata, viewport } from 'next-sanity/studio'
-
 export default function StudioPage() {
   return <Studio config={config} />
 }
